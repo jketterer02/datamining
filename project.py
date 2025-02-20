959b2e6433bb2ef59f327299e2127276631f75eb
Jacob Ketterer <jketterer02@gmail.com>
2025-02-20 17:12:18 -0600

    add parser

---
+++ project.py
@@ -0,0 +1,23 @@
+import csv 
+import os
+
+
+def parse_csv(file_path):
+    if not os.path.exists(file_path):raise FileNotFoundError(f"The file '{file_path}' was not found.")
+    
+    data = []
+    with open(file_path, 'r') as file:
+        reader = csv.DictReader(file, delimiter=';')
+        for row in reader:
+            parsed_row = {}
+            for key, value in row.items():
+                # Gives actual values
+                if value.isdigit(): parsed_row[key] = int(value)
+                else: parsed_row[key] = value.strip('"')
+            data.append(parsed_row)
+    return data
+
+#Get the data
+data = parse_csv('student-mat.csv')
+#Print first 5 rows
+print(data[:5])
